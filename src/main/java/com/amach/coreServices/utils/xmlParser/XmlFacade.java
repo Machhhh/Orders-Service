2d18f9911687022a1a4a6dcfa1b6a1680eaa61ca
Artur M <machowicz.artur@gmail.com>
2018-10-07 22:07:52 +0200

    Create Xml Facade
    *Added Xml config

---
+++ src/main/java/com/amach/coreServices/utils/xmlParser/XmlFacade.java
@@ -0,0 +1,34 @@
+package com.amach.coreServices.utils.xmlParser;
+
+import com.amach.coreServices.request.RequestDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class XmlFacade {
+
+    private XmlService xS;
+
+    public void saveToXml(final List<RequestDto> dtoList)
+            throws JAXBException, IOException {
+        xS.saveToXmlFile(xS.createXmlRequests(dtoList));
+    }
+
+    public void saveToXmlByClientId(final List<RequestDto> dtoList,
+                                    final Long clientId)
+            throws JAXBException, IOException {
+        xS.saveToXmlByClientId(xS.createXmlRequestsByClientId(
+                dtoList, clientId), clientId);
+    }
+
+    public XmlRequests parseFromXmlFile(final File myFile)
+            throws JAXBException {
+        return xS.loadFromXmlFile(myFile);
+    }
+}
