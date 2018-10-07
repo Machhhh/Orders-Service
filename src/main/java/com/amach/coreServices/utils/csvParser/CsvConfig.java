81244e58dde3cfef9bd3e6d6673ea34e36eaa142
Artur M <machowicz.artur@gmail.com>
2018-10-07 22:04:30 +0200

    Create Csv Facade
    *Added Csv config

---
+++ src/main/java/com/amach/coreServices/utils/csvParser/CsvConfig.java
@@ -0,0 +1,13 @@
+package com.amach.coreServices.utils.csvParser;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CsvConfig {
+
+    @Bean
+    CsvFacade csvFacade(final CsvService csvService) {
+        return new CsvFacade(csvService);
+    }
+}
---
+++ src/main/java/com/amach/coreServices/utils/csvParser/CsvFacade.java
@@ -0,0 +1,34 @@
+package com.amach.coreServices.utils.csvParser;
+
+import com.amach.coreServices.request.RequestDto;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class CsvFacade {
+
+    private CsvService cS;
+
+    public List<RequestDto> parseFromCsvFile(final File myFile)
+            throws IOException {
+        return cS.parseFromCsvFile(myFile);
+    }
+
+    public void saveToCsv(final List<RequestDto> dtoList) throws IOException,
+            CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+        cS.saveToCsv(dtoList);
+    }
+
+    public void saveToCsvByClientId(final List<RequestDto> dtoList,
+                                    final Long clientId) throws IOException,
+            CsvDataTypeMismatchException, CsvRequiredFieldEmptyException {
+        cS.saveToCsvByClientId(dtoList, clientId);
+    }
+}
