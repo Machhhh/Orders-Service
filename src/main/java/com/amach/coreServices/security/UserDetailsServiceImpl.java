35cb2256d3dbded1c39b71f7742eb16838c49621
Artur M <machowicz.artur@gmail.com>
2018-10-26 15:16:14 +0200

    Create userd details impl
    *Added data.sql for testing

---
+++ src/main/java/com/amach/coreServices/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.amach.coreServices.security;
+
+import com.amach.coreServices.client.Client;
+import com.amach.coreServices.client.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+class UserDetailsServiceImpl implements UserDetailsService {
+
+    private ClientRepository clientRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Client client = clientRepository.findByLogin(login);
+        if (client == null) {
+            throw new UsernameNotFoundException("Client not found");
+        }
+
+        return new UserDetailsImpl(client.getLogin(), client.getPassword(),
+                client.getRole(), client.getId());
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+/*insert into authorities
+values(1,'1970-01-01 00:00:01.00','1985-08-18 05:30:00.00',
+'bite my code :)','ADMIN');*/
+insert into clients
+values(1,'1970-01-01 00:00:01.00','1985-08-18 05:30:00.00',
+'bite my code :)','ordersservice@gmail.com', 'admin', 'mach',
+ '$2a$10$7RJ9T9lLYCfqXDDENJVYy.8UPv8ssi3PTsWNdkawCg7q7E8EIRxHS','ROLE_ADMIN');
+insert into clients
+values(2,'1970-01-01 00:00:01.00','1985-08-18 05:30:00.00',
+'review my code :)','machfejs@gmail.com', 'user', 'mach',
+ '$2a$10$7RJ9T9lLYCfqXDDENJVYy.8UPv8ssi3PTsWNdkawCg7q7E8EIRxHS','ROLE_USER');
+
